99c57690e50e14dda7fcb8b9d1cdc5843283914a
gavinsunde <gavinsunde@gmail.com>
2017-02-05 14:50:51 -0600

    Update to start st0 instead of st.

--- config.def.h
+++ config.def.h
@@ -2,9 +2,9 @@
 
 /* appearance */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Fira Mono:size=10"
 };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "Fira Mono:size=10";
 static const char normbordercolor[] = "#839496"; /* s=base0 */
 static const char normbgcolor[]     = "#002b36"; /* s-base03 "#222222"; */
 static const char normfgcolor[]     = "#839496"; /* s-base0 */
@@ -55,7 +55,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st0", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
