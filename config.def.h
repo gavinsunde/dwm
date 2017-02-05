768185f0434f7d6e8e7febdd631336e094e0b28f
gavinsunde <gavinsunde@gmail.com>
2017-02-05 13:45:20 -0600

    Update dmenu colors.

--- config.def.h
+++ config.def.h
@@ -5,12 +5,12 @@ static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char dmenufont[]       = "monospace:size=10";
-static const char normbordercolor[] = "#444444";
-static const char normbgcolor[]     = "#222222";
-static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#005577";
-static const char selbgcolor[]      = "#005577";
-static const char selfgcolor[]      = "#eeeeee";
+static const char normbordercolor[] = "#839496"; /* s=base0 */
+static const char normbgcolor[]     = "#002b36"; /* s-base03 "#222222"; */
+static const char normfgcolor[]     = "#839496"; /* s-base0 */
+static const char selbordercolor[]  = "#859900"; /* s-green "#005577"; */
+static const char selbgcolor[]      = "#859900"; /* s-green "#005577"; */
+static const char selfgcolor[]      = "#073642"; /* s-base02 "#eeeeee"; */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
